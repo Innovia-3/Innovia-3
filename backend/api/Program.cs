fe2d4904fe40487803a1f84ac2c9bfc74d297697
C-Friberg <you@example.com>
2026-08-31 11:44:12 +0200

    AppDbContext setup

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,9 +1,16 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("defaultConnection"));
+});
 
 var app = builder.Build();
 
