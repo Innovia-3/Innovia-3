9e0b09d42111eed0e608bd613e1b7b209032d1b1
Fabian Tholin <fabiantholin@proton.me>
2026-09-04 11:52:10 +0200

    Namnbyten

--- backend/api/Dtos/AuthDtos/RegisterUserDto.cs
+++ backend/api/Dtos/AuthDtos/RegisterUserDto.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Dtos.UserDtos
 {
     /* för admin vid skapande av ny användare */
     public class RegisterUserDto
-    {
+    {   
         public string UserName { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
