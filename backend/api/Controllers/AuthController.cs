0750234df76765634ef09e063dafb380c10101f6
Elaheh Hamednezhad <elaheh@Elahehs-MacBook-Air.local>
2026-09-09 22:44:23 +0200

    Implement login with JWT authentication

--- backend/api/Controllers/AuthController.cs
+++ backend/api/Controllers/AuthController.cs
@@ -1,23 +1,24 @@
 using api.Dtos.UserDtos;
 using api.Interfaces;
 using api.Models;
-using Microsoft.AspNetCore.Identity;
+using api.Services;
 using Microsoft.AspNetCore.Mvc;
-/* using Microsoft.AspNetCore.Authorization; används när JWT är implementerat */
 
 namespace api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-
-    /* [Authorize (Roles = "Admin")] läggs till när JWT är implementerat*/
     public class AuthController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
+        private readonly TokenService _tokenService;
 
-        public AuthController(IUserRepository userRepository)
+        public AuthController(
+            IUserRepository userRepository,
+            TokenService tokenService)
         {
             _userRepository = userRepository;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
@@ -29,7 +30,10 @@ namespace api.Controllers
                 UserName = registerUserDto.Email
             };
 
-            var created = await _userRepository.CreateUserAsync(user, registerUserDto.Password);
+            var created = await _userRepository.CreateUserAsync(
+                user,
+                registerUserDto.Password
+            );
 
             if (!created)
             {
@@ -37,6 +41,31 @@ namespace api.Controllers
             }
 
             return Ok("Användaren skapades");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _userRepository.GetByEmailAsync(loginDto.Email);
+
+            if (user == null)
+            {
+                return Unauthorized("Fel email eller lösenord.");
+            }
+
+            var passwordCorrect = await _userRepository.CheckPasswordAsync(
+                user,
+                loginDto.Password
+            );
+
+            if (!passwordCorrect)
+            {
+                return Unauthorized("Fel email eller lösenord.");
+            }
+
+            var token = _tokenService.CreateToken(user);
+
+            return Ok(new { token });
         }
     }
 }
