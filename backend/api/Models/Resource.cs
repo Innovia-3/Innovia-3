d7d97390b9560cd986e2ef21befd6e190ff13169
Fabian Tholin <fabiantholin@proton.me>
2026-09-11 15:01:58 +0200

    Rättning av föregående commit (och PR)

--- backend/api/Models/Resource.cs
+++ backend/api/Models/Resource.cs
@@ -11,35 +11,6 @@ namespace api.Models
     {
         public int ResourceId { get; set; }
         public ResourceType ResourceType { get; set; }
-        private int _timeSlotTimeInHours;
-        public int TimeSlotTimeInHours
-        {
-            get
-            {
-                return _timeSlotTimeInHours;
-            }
-            set
-            {
-                switch (ResourceType)
-                {
-                    case ResourceType.Skrivbord:
-                        _timeSlotTimeInHours = 3;
-                        break;
-                    case ResourceType.Mötesrum:
-                        _timeSlotTimeInHours = 3;
-                        break;
-                    case ResourceType.VRHeadset:
-                        _timeSlotTimeInHours = 1;
-                        break;
-                    case ResourceType.AIServer:
-                        _timeSlotTimeInHours = 1;
-                        break;
-                    default:
-                        _timeSlotTimeInHours = 1;
-                        break;
-                }
-            }
-        }
         public int? Capacity { get; set; }
         public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
         public ICollection<Sensor> Sensors { get; set; } = new List<Sensor>();
