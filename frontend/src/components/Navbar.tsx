7601356bf6c22030d889bfb44582ea91da20471f
Elaheh Hamednezhad <elaheh@Elahehs-MacBook-Air.local>
2026-09-15 20:07:13 +0200

    Lägg till Mina bokningar i navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,31 +1,41 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./css/Navbar.module.css";
 
 export default function Navbar() {
-    const navigate = useNavigate();
-    const [isLoggedIn, setIsLoggedIn] = useState(
-        !!localStorage.getItem("token")
-    );
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
 
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        setIsLoggedIn(false);
-        navigate("/login");
-    };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
 
-    return (
-        <nav className={styles.navbar}>
-            <h1 className={styles.logo}>Innovia</h1>
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.logo}>Innovia</h1>
 
-            <button
-                className={styles.loginButton}
-                onClick={() =>
-                    isLoggedIn ? handleLogout() : navigate("/login")
-                }
-            >
-                {isLoggedIn ? "Logga ut" : "Logga in"}
-            </button>
-        </nav>
-    );
+      <div>
+        {isLoggedIn && location.pathname === "/" && (
+          <button
+            className={styles.loginButton}
+            onClick={() =>
+              document.getElementById("bookings")?.scrollIntoView()
+            }
+          >
+            Mina bokningar
+          </button>
+        )}
+
+        <button
+          className={styles.loginButton}
+          onClick={() => (isLoggedIn ? handleLogout() : navigate("/login"))}
+        >
+          {isLoggedIn ? "Logga ut" : "Logga in"}
+        </button>
+      </div>
+    </nav>
+  );
 }
