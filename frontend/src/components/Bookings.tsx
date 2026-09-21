213f99623739ae563dbcddbbd1adc4a98be5e318
C-Friberg <you@example.com>
2026-09-21 14:34:45 +0200

    replace bookingid to resourceid

--- frontend/src/components/Bookings.tsx
+++ frontend/src/components/Bookings.tsx
@@ -119,8 +119,8 @@ export default function Bookings() {
         </div>
 
         <div className={styles.info}>
-          <div className={styles.bookingId}>ID</div>
           <div className={styles.resource}>Resurs</div>
+          <div className={styles.resourceId}>ID</div>
           <div className={styles.user}>Användare</div>
           <div className={styles.date}>Datum</div>
           <div className={styles.time}>Tid</div>
@@ -149,8 +149,8 @@ export default function Bookings() {
         <div className={styles.bookingList}>
           {visibleBookings.map((booking) => (
             <div key={booking.bookingId} className={styles.bookingRow}>
-              <div className={styles.bookingId}>{booking.bookingId}</div>
               <div className={styles.resource}>{booking.resourceType}</div>
+              <div className={styles.resourceId}>{booking.resourceId}</div>
 
               <div className={styles.user}>{booking.userEmail}</div>
 
